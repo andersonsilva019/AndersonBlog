ec3a3dee4f20b855cd2590a0ae92d13845673edc
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-04-22 21:49:30 -0300

    feat: adicionando eslint prettier editorconfig

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
         </Head>
         <body>
