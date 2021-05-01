0328e8343c333dd57c31d8493c1e4e49f132c57a
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-05-01 19:30:29 -0300

    access token prismic

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,8 +1,10 @@
 import Prismic from '@prismicio/client'
 export function prismicClient(req?: unknown) {
   const prismic = Prismic.client(
-    process.env.PRISMIC_ENDPOINT,
-    { req }
+    process.env.PRISMIC_ENDPOINT, {
+    req,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN
+  }
   )
   return prismic
 }
