0baf8a0ce3ed3ca117ace74f63e44683a492e7b1
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-07-07 10:45:31 -0300

    update script googleAds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Layout } from '../Layout'
 
 import '../styles/global.scss'
@@ -6,6 +7,9 @@ import '../styles/global.scss'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <script data-ad-client="ca-pub-9324248521098640" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,10 +8,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
-          <script data-ad-client="ca-pub-9324248521098640" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <link rel="canonical" href="https://personnel-blog.vercel.app/" key="canonical" />
         </Head>
         <body>
