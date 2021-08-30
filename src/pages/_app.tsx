d4877a881910e84078f3899ddc84fa052a017876
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-08-30 09:16:05 -0300

    chore: atualizando script de adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app'
-import Head from 'next/head'
 import { Layout } from '../Layout'
 
 import '../styles/global.scss'
@@ -7,9 +6,6 @@ import '../styles/global.scss'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Head>
-        <script data-ad-client="ca-pub-9324248521098640" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      </Head>
       <Component {...pageProps} />
     </Layout>
   )
