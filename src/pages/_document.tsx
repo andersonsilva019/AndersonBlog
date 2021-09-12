f5e9eaff7cbc68cc67c325c3f12028971f59f6a7
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-12 12:14:14 -0300

    chore: removendo comentario da secao adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
-          {/* Google adsense */}
           <script data-ad-client="ca-pub-9324248521098640" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="canonical" href="https://personnel-blog.vercel.app/" key="canonical" />
