bb40632e07496b8f8cdf0deaaa469c5ab227e862
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-22 16:12:06 -0300

    bundle analyze

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['images.prismic.io', 'site-blog.cdn.prismic.io'],
   },
-}
+});
