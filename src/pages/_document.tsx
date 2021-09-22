c82318b217f997885a1393e743fc5c6c356d42a7
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-22 11:57:05 -0300

    SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9324248521098640"
-            crossOrigin="anonymous"></script>
+          <script data-ad-client="ca-pub-9324248521098640" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="canonical" href="https://personnel-blog.vercel.app/" key="canonical" />
         </Head>
