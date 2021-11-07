4abcf65a3f4a0a04d2a915333e19328e55827f82
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-11-07 20:17:55 -0300

    SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,17 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
-          <script data-ad-client="ca-pub-9324248521098640" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-               (adsbygoogle = window.adsbygoogle || []).push({
-                   google_ad_client: "ca-pub-9324248521098640",
-                   enable_page_level_ads: true
-              });
-                `
-            }}
-          />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3869788745321127"></script>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="canonical" href="https://andersonnsilva.site/" key="canonical" />
         </Head>
