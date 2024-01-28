9bb6906cfcc6a376bc8ff6eec1640e6a20d0f9ca
Anderson Silva <andersonnsilva@alu.ufc.br>
2024-01-28 16:20:08 -0300

    Remove Google AdSense script from _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
           <meta charSet="utf-8" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3869788745321127" crossOrigin="anonymous"></script>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="canonical" href="https://andersonnsilva.site/" key="canonical" />
         </Head>
