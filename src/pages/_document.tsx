3066b8cbef13c984a8daf1c4010c2d1df4c7655f
Anderson Silva <andersonnsilva@alu.ufc.br>
2024-01-28 16:39:25 -0300

    Update canonical URL in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
-          <link rel="canonical" href="https://andersonnsilva.site/" key="canonical" />
+          <link rel="canonical" href="https://personnel-blog.vercel.app/" key="canonical" />
         </Head>
         <body>
           <Main />
